6fc6cbd61d5cd799a9f6cba698928a343b12592d
yiseul_notebook <kimdew402@gmail.com>
2023-11-07 16:34:19 +0900

    자료의 순서가 변경되면 상세페이지도 고장나는 문제 해결 방법 1

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -5,22 +5,35 @@ import { useParams } from 'react-router-dom';
 function Detail(props) {
 
     // useParams라는 hook을 이용해 api에 적힌 id=숫자(App.js 에 있는 :id 값) 값을 받아올 수 있다. 
-    let {id} = useParams();
+    let { id } = useParams();
+    let img = id - 1;
 
+
+    //가나다 정렬시 상세페이지가 불규칙해지는 문제 해결하기
     return (
-    <Container>
-        <Row>
-            <Col>
-                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </Col>
-            <Col>
-                <h4 className="py-5">{props.shoes[id].title}</h4>
-                <p>{props.shoes[id].content}</p>
-                <p>{props.shoes[id].price}원</p>
-                <Button variant="danger">주문하기</Button>
-            </Col>
-        </Row>
-    </Container>
+        <Container>
+            <Row>
+                <Col>
+                    <img src={'https://codingapple1.github.io/shop/shoes' + id + '.jpg'} width="100%" />
+                </Col>
+                <Col>
+                    {props.shoes.map(shoe => {
+                        if (shoe.id === img) {
+                            return (
+                                <div key={shoe.id}>
+                                    <h4 className="py-5">{shoe.title}</h4>
+                                    <p>{shoe.content}</p>
+                                    <p>{shoe.price}원</p>
+                                    <Button variant="danger">주문하기</Button>
+                                </div>
+                            );
+                        }
+                        return null;
+                    })}
+
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
