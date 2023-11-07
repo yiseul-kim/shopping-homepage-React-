f2d58083fefefd9075195a1dd32dc980317d6825
yiseul_notebook <kimdew402@gmail.com>
2023-11-07 11:50:55 +0900

    코드 길어지면 import export 하면 됩니다.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import data from './data';
 
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -23,18 +28,18 @@ function App() {
             {/* process.env.PUBLIC_URL로 경로를 자세히 설정 : 만약 페이지 경로를 /와 같은 
             상대경로로 잡을 경우 서브페이지에 발행되거나 할 때 자신의 파일 경로를 잡지 못하는 경우 */}
             <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </Col>
           <Col>
             <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </Col>
           <Col>
             <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
           </Col>
         </Row>
       </Container>
