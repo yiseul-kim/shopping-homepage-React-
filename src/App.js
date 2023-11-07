1c8087cb3d615226013e8f7e95d5abeac5be79c9
yiseul_notebook <kimdew402@gmail.com>
2023-11-07 15:33:05 +0900

    리액트 라우터 3 : URL 파라미터로 사세페이지 100개 만들기

--- src/App.js
+++ src/App.js
@@ -44,44 +44,13 @@ function App() {
             </Container>
           </>}
         />
-
-        <Route path="/detail" element={<Detail />} />
-
-        {/* 지정된 페이지 이외에 접속일때 없는 페이지라는 문구 띄우기 */}
-        <Route path="*" element={<div> 없는 페이지 입니다. (404 Error) </div>} />
-
-        {/* nested Routes */}
-        <Route path="/about" element={<About />} >
-          <Route path="member" element={<div> 멤버 </div>} />  {/* /about/member */}
-          <Route path="location" element={<div> 위치정보 </div>} />  {/* /about/location */}
-        </Route>
-
-        <Route path="/event" element={<Event />} >
-          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
-          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
-        </Route>
         
+        {/* url 파라미터 */}
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+
       </Routes>
     </div>
   );
-}
-
-function About() {
-  return (
-    <div>
-      <h4>회사 정보임</h4>
-      <Outlet></Outlet>
-    </div>
-  )
-}
-
-function Event() {
-  return (
-    <div>
-      <h4>오늘의 이벤트</h4>
-      <Outlet></Outlet>
-    </div>
-  )
 }
 
 function Card(props) {
--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
-function Detail() {
+function Detail(props) {
+
+    // useParams라는 hook을 이용해 api에 적힌 id=숫자(App.js 에 있는 :id 값) 값을 받아올 수 있다. 
+    let {id} = useParams();
+
     return (
     <Container>
         <Row>
@@ -9,9 +14,9 @@ function Detail() {
                 <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
             </Col>
             <Col>
-                <h4 className="py-5">상품명</h4>
-                <p>상품설명</p>
-                <p>120000원</p>
+                <h4 className="py-5">{props.shoes[id].title}</h4>
+                <p>{props.shoes[id].content}</p>
+                <p>{props.shoes[id].price}원</p>
                 <Button variant="danger">주문하기</Button>
             </Col>
         </Row>
