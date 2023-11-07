dd2ce5ab9b8e8fcac7c69e8b9a3a9818e70f3d1c
yiseul_notebook <kimdew402@gmail.com>
2023-11-07 17:02:06 +0900

    styled-components 쓰면 CSS 파일 없어도 되는데

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+// styled-components 사용하기
+// App.module.css 로 작명해서 만들면 App.js에서만 사용가능 함 (종속된다.)
+let RedBtn = styled.button`
+    background : red;
+    color : white;
+    padding : 10px;
+    width : 100px;
+`
+// 백그라운드 색상 재활용 하기
+let RedBtn1 = styled.button`
+    background : ${props => props.bg};
+    color : ${props => props.bg == 'blue' ? 'black' : 'white' };
+    padding : 10px;
+    width : 100px;
+`
+
+let Box = styled.div`
+    background : grey;
+    padding : 20px;
+    width : 150px;
+`
+
 
 function Detail(props) {
 
@@ -15,6 +40,10 @@ function Detail(props) {
     //가나다 정렬시 상세페이지가 불규칙해지는 문제 해결하기
     return (
         <Container>
+            <Box>
+                <RedBtn>버튼</RedBtn>
+                <RedBtn1 bg="blue">버튼</RedBtn1>
+            </Box>
             <Row>
                 <Col>
                     <img src={'https://codingapple1.github.io/shop/shoes' + id + '.jpg'} width="100%" />
